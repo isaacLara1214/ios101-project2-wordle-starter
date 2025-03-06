95bbb182e32b47e21382d64d76a6152491557404
Isaac Lara <isaac.lara014@gmail.com>
2025-03-05 20:10:24 -0500

    completed exercise 1-4

--- Wordle/BoardController.swift
+++ Wordle/BoardController.swift
@@ -62,7 +62,9 @@ class BoardController: NSObject,
   // Checkpoint: Correctly implementing this should allow you to change the number of letters in the goal word!
   private func applyNumLettersSettings(with settings: [String: Any]) {
     // START YOUR CODE HERE
-    // ...
+      if let numLetters = settings [kNumLettersKey] as? Int {
+          numItemsPerRow = numLetters
+      }
     // END YOUR CODE HERE
   }
   
@@ -74,7 +76,10 @@ class BoardController: NSObject,
   // Checkpoint: Correctly implementing this should allow you to change the number of rows in the board!
   private func applyNumGuessesSettings(with settings: [String: Any]) {
     // START YOUR CODE HERE
-    // ...
+      if let numRow = settings [kNumGuessesKey] as? Int {
+          self.numRows = numRow
+          print(settings)
+      }
     // END YOUR CODE HERE
   }
   
@@ -87,7 +92,11 @@ class BoardController: NSObject,
   // to check the before/after value of goalWord and see if it changes to the correct theme
   private func applyThemeSettings(with settings: [String: Any]) {
     // START YOUR CODE HERE
-    // ...
+      if let themeString = settings[kWordThemeKey] as? String {
+          if let theme = WordTheme(rawValue: themeString) {
+              self.goalWord = WordGenerator.generateGoalWord(with: theme)
+          }
+      }
     // END YOUR CODE HERE
   }
   
@@ -97,7 +106,9 @@ class BoardController: NSObject,
   // Checkpoint: Correctly implementing this function should change the goal word each time the user inputs an entire row of letters
   private func applyIsAlienWordleSettings(with settings: [String: Any]) {
     // START YOUR CODE HERE
-    // ...
+      if let alienMode = settings[kIsAlienWordleKey] as? Bool {
+          isAlienWordle = alienMode
+      }
     // START YOUR CODE HERE
   }
 }
